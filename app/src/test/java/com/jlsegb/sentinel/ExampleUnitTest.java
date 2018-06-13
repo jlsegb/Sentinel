c19941c86e930c23ddba81893cb8d2c5083b6c84
jlsegb <jlsegb@gmail.com>
2018-06-13 14:43:13 -0600

    Adding some tests

--- app/src/test/java/com/jlsegb/sentinel/ExampleUnitTest.java
+++ app/src/test/java/com/jlsegb/sentinel/ExampleUnitTest.java
@@ -12,6 +12,22 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
+
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void registeringTest(){
+
+    }
+
+    @Test
+    public void matchingTest(){
+
+    }
+
+    @Test
+    public void informationUpdateTest(){
+
     }
 }
