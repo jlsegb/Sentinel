28a6c2737c8a5bcba92035ef35a1d9bd32673fdc
Korzahk <ecc16001@byui.edu>
2018-06-15 15:48:46 -0600

    Added DB and Controller classes

---
+++ app/src/main/java/com/jlsegb/sentinel/Controller.java
@@ -0,0 +1,12 @@
+package com.jlsegb.sentinel;
+
+import org.json.JSONObject;
+
+public class Controller {
+
+    private DB db;
+
+    public JSONObject toJSON(String id) {
+        return null;
+    }
+}
